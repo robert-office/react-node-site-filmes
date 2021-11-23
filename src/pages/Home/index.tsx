b17f4064f8d23b157315d7c6c290f8f99739890d
robert-office <robertuillians123@gmail.com>
2021-11-23 15:05:11 -0300

    Header novo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,9 @@
-import { NavBar } from "components/NavBar";
-
+import { NavBarHeader } from "components/NavBar";
 
 const Home = () => {
   return (
     <>
-      <NavBar/>
-      
-     
+      <NavBarHeader />
     </>
   );
 };
