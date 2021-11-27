8aa3faa9db438efd78f06474f137c809727a71e1
robert-office <robertuillians123@gmail.com>
2021-11-27 13:05:37 -0300

    Tela de cadastro já foi modifica e finalizada, esperando apenas a implementação da API

--- src/components/Cadastro/index.tsx
+++ src/components/Cadastro/index.tsx
@@ -1,3 +1,5 @@
+import svg from "../../assets/images/svgs/assistindoVideos.svg";
+
 export const CadastroForm = () => {
   return (
     <>
@@ -8,43 +10,29 @@ export const CadastroForm = () => {
             <div className="relative w-full bg-cover lg:w-6/12 xl:w-7/12 bg-gradient-to-r from-white via-white to-gray-100">
               <div className="relative flex flex-col items-center justify-center w-full h-full px-10 my-20 lg:px-16 lg:my-0">
                 <div className="flex flex-col items-start space-y-8 tracking-tight lg:max-w-3xl">
-                  <div className="relative">
-                    <p className="mb-2 font-medium text-gray-700 uppercase">
-                      Work smarter
-                    </p>
-                    <h2 className="text-5xl font-bold text-gray-900 xl:text-6xl">
-                      Features to help you work smarter
-                    </h2>
-                  </div>
-                  <p className="text-2xl text-gray-700">
-                    We've created a simple formula to follow in order to gain
-                    more out of your business and your application.
-                  </p>
-                  <a
-                    href="#_"
-                    className="inline-block px-8 py-5 text-xl font-medium text-center text-white transition duration-200 bg-blue-600 rounded-lg hover:bg-blue-700 ease"
-                  >
-                    Get Started Today
-                  </a>
+                  
+                  <img src={svg} alt="svg" />
+                  
                 </div>
               </div>
             </div>
             <div className="w-full bg-white lg:w-6/12 xl:w-5/12">
               <div className="flex flex-col items-start justify-start w-full h-full p-10 lg:p-16 xl:p-24">
-                <h4 className="w-full text-3xl font-bold">Signup</h4>
+                <h4 className="w-full text-3xl font-bold">Cadastre-se</h4>
                 <p className="text-lg text-gray-500">
-                  or, if you have an account you can{" "}
+                  ou, então você pode{" "}
                   <a href="#_" className="text-blue-600 underline">
-                    sign in
+                    logar-se
                   </a>
+                  , se já possuir uma conta
                 </p>
                 <div className="relative w-full mt-10 space-y-8">
                   <div className="relative">
-                    <label className="font-medium text-gray-900">Name</label>
+                    <label className="font-medium text-gray-900">Nome</label>
                     <input
                       type="text"
                       className="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50"
-                      placeholder="Enter Your Name"
+                      placeholder="Coloque Seu Nome"
                     />
                   </div>
                   <div className="relative">
@@ -52,31 +40,31 @@ export const CadastroForm = () => {
                     <input
                       type="text"
                       className="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50"
-                      placeholder="Enter Your Email Address"
+                      placeholder="Coloque Seu Endereço De Email"
                     />
                   </div>
                   <div className="relative">
                     <label className="font-medium text-gray-900">
-                      Password
+                      Senha
                     </label>
                     <input
                       type="password"
                       className="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50"
-                      placeholder="Password"
+                      placeholder="Coloque Sua Senha"
                     />
                   </div>
                   <div className="relative">
                     <a
                       href="#_"
-                      className="inline-block w-full px-5 py-4 text-lg font-medium text-center text-white transition duration-200 bg-blue-600 rounded-lg hover:bg-blue-700 ease"
+                      className="inline-block w-full px-5 py-4 text-lg font-medium text-center text-white transition duration-200 bg-indigo-600 rounded-lg hover:bg-blue-700 ease"
                     >
-                      Create Account
+                      Criar Conta Grátis
                     </a>
                     <a
                       href="#_"
                       className="inline-block w-full px-5 py-4 mt-3 text-lg font-bold text-center text-gray-900 transition duration-200 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 ease"
                     >
-                      Sign up with Google
+                      Cadastro Através do Google
                     </a>
                   </div>
                 </div>
