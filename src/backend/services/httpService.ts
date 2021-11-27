482f06f0b8ab1f4330fa43be4ce6ee0e2b562416
robert-office <robertuillians123@gmail.com>
2021-11-27 15:50:37 -0300

    Adicionado e reparado os btns de navegação do SwipperJS

--- src/backend/services/httpService.ts
+++ src/backend/services/httpService.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 class httpService{
     execute(){
         return axios.create({
-            baseURL: `${process.env.REACT_APP_BASE_URL}/api`
+            baseURL: `${process.env.REACT_APP_BASE_API_URL}/api`
         });
     }
 }
--- src/components/LocalCardsSwipper/index.tsx
+++ src/components/LocalCardsSwipper/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper } from "swiper/react";
 // modules styles
 import "swiper/components/navigation/navigation.min.css";
 import "swiper/components/pagination/pagination.min.css";
@@ -9,23 +9,53 @@ import "swiper/swiper-bundle.min.css";
 // swiper core styles
 import "swiper/swiper.min.css";
 // import Swiper core and required modules
-import SwiperCore, { Pagination } from "swiper";
+import SwiperCore, { Navigation, Pagination } from "swiper";
+
+import arrowR from "../../assets/images/svgs/ArrowR.svg";
+import arrowL from "../../assets/images/svgs/ArrowL.svg";
 
 // install Swiper modules
-SwiperCore.use([Pagination]);
+SwiperCore.use([Pagination, Navigation]);
 
 export const LocalCardsSwipper = class Grid extends React.Component {
   render() {
+    const next =
+      "swiper-button-next-" + String(Math.floor(Math.random() * 100));
+    const prev =
+      "swiper-button-prev-" + String(Math.floor(Math.random() * 100));
+
     return (
-      <div className="flex pb-10 sm:px-5 gap-x-8 gap-y-16">
+      <div className="relative flex pb-10 sm:px-5 gap-x-8 gap-y-16">
+        <div className="absolute z-20 w-full h-1 flex justify-between top-10">
+          <button
+            className={
+              prev +
+              " animate-pulse relative h-12 w-12 rounded-full bg-indigo-600 text-center text-white font-extrabold"
+            }
+          >
+            <img src={arrowL} alt="prev" />
+          </button>
+
+          <button
+            className={
+              next +
+              " animate-pulse relative sm:mr-10  h-10 w-10 rounded-full bg-indigo-600 text-center text-white font-extrabold"
+            }
+          >
+            <img src={arrowR} alt="next" />
+          </button>
+        </div>
+
         <Swiper
           slidesPerView={1}
           spaceBetween={30}
           pagination={{
             clickable: true,
-            type: "progressbar",
           }}
-          navigation={true}
+          navigation={{
+            nextEl: "." + next,
+            prevEl: "." + prev,
+          }}
           breakpoints={{
             490: {
               slidesPerView: 1,
@@ -42,7 +72,7 @@ export const LocalCardsSwipper = class Grid extends React.Component {
               spaceBetween: 20,
             },
 
-            1200: {
+            1100: {
               slidesPerView: 4,
               spaceBetween: 20,
             },
@@ -52,6 +82,9 @@ export const LocalCardsSwipper = class Grid extends React.Component {
               spaceBetween: 20,
             },
           }}
+          observer={true}
+          observeParents={true}
+          parallax={true}
           className="mySwiper"
         >
           {this.props.children}
