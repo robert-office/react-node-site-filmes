880712fab7c4ef6afee76211e22ce934f973f7bc
robert-office <robertuillians123@gmail.com>
2021-12-23 16:03:03 -0300

    dashboard configurations com as tabs

--- src/components/dashboardContainer/index.tsx
+++ src/components/dashboardContainer/index.tsx
@@ -1,33 +1,14 @@
 import React from "react";
 import { MenuDashboard } from "components/MenuDashboard";
+import { Tabs } from "react-tabs";
 
 export class DashboardContainer extends React.Component {
     render() {
         return (
 
-            <section className="w-full px-8">
-                <div className="container flex flex-col flex-wrap items-center justify-between py-5 mx-auto md:flex-row max-w-7xl">
-                    <div className="w-full">
-                        <main className="bg-gray-100 dark:bg-gray-800 rounded-2xl h-auto overflow-hidden relative container py-5">
-                            <div className="flex items-start justify-between">
-                                <div className="h-auto hidden lg:block my-4 shadow-lg relative w-80">
-                                    <div className="bg-white h-full rounded-2xl dark:bg-gray-700">
-                                        <MenuDashboard />
-                                    </div>
-                                </div>
-                                <div className="flex flex-col w-full pl-0 md:py-4 md:space-y-4">
-
-                                    <div className="overflow-auto h-auto pb-24 pt-2">
-                                        <div className="flex flex-col flex-wrap sm:flex-row ">
-                                            {this.props.children}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </main>
-                    </div>
-                </div>
-            </section>
+            <Tabs>
+                <MenuDashboard />
+            </Tabs>
         );
     }
 }
--- src/components/menuDashboard/index.tsx
+++ src/components/menuDashboard/index.tsx
@@ -1,5 +1,5 @@
 import AccountBoxIcon from '@material-ui/icons/AccountBox';
-import { NavLink } from 'react-router-dom';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 type list = {
     menuName: string;
@@ -13,44 +13,44 @@ type menuArray = {
 }
 
 export const MenuDashboard = () => {
-    
+
     const menu: menuArray = {
         menus: [
             {
                 menuName: "Home",
-                menuHref: process.env.REACT_APP_BASE_URL+"/dashboard",
+                menuHref: process.env.PUBLIC_URL + "/dashboard",
                 svg: <AccountBoxIcon />
             },
             {
                 menuName: "Settings",
-                menuHref: process.env.REACT_APP_BASE_URL+"/dashboard/settings",
+                menuHref: process.env.PUBLIC_URL + "/dashboard/settings",
                 svg: <AccountBoxIcon />
             },
             {
                 menuName: "WatchList",
-                menuHref: process.env.REACT_APP_BASE_URL+"/dashboard/watchlist",
+                menuHref: process.env.PUBLIC_URL + "/dashboard/watchlist",
                 svg: <AccountBoxIcon />
             },
             {
                 menuName: "Favorites",
-                menuHref: process.env.REACT_APP_BASE_URL+"/dashboard/favorites",
+                menuHref: process.env.PUBLIC_URL + "/dashboard/favorites",
                 svg: <AccountBoxIcon />
             }
         ]
     }
-    
+
     return (
-        <nav className="mt-6">
-
-            {menu.menus.map((menu) => {
-                return (
-                    <div>
-                        <DashboardList menuList={menu}/>
-                    </div>
-                );
-            })}
-
-        </nav>
+        <section className="w-full px-8 text-gray-700 bg-white dark:bg-gray-800">
+            <TabList className="container flex flex-col flex-wrap items-center py-5 mx-auto md:flex-row max-w-7xl">
+                {menu.menus.map((menu) => {
+                    return (
+                        <div className="w-auto">
+                            <Tab><DashboardList menuList={menu} /></Tab>
+                        </div>
+                    );
+                })}
+            </TabList>
+        </section>
     );
 }
 
@@ -58,16 +58,17 @@ type Props = {
     menuList: list
 }
 
-export const DashboardList = ({menuList} : Props) => {
+export const DashboardList = ({ menuList }: Props) => {
     return (
-        <a
-        className="w-full font-thin uppercase text-blue-500 flex items-center p-4 my-2 transition-colors duration-200 justify-start bg-gradient-to-r from-white to-blue-100 border-r-4 border-blue-500 dark:from-gray-700 dark:to-gray-800 "
-        href={menuList.menuHref}
+        <button
+            className="w-40 text-indigo-600 dark:text-white flex items-center p-4 transition-colors duration-200 
+            justify-start md:bg-gradient-to-t bg-gradient-to-r from-white to-blue-100 border-b-4 border-indigo-600 dark:from-gray-700 dark:to-gray-800
+            hover:text-blue-400"
         >
-            <span className="text-left">
-                {menuList.svg}
+            <span className="text-left font-extrabold">
+                <p>{menuList.svg}</p>
             </span>
-            <span className="mx-4 text-sm font-normal">{menuList.menuName}</span>
-        </a>
+            <span className="mx-4 text-sm font-bold">{menuList.menuName}</span>
+        </button>
     );
 }
