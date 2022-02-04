f311a76dc64ba1e17db8bd45d0063db2b55b231c
robert-office <robertuillians123@gmail.com>
2022-02-04 16:17:38 -0300

    add: conteudo da tela de favoritos e da tela de watchlist

--- src/pages/dashboardFavoritesPage/index.tsx
+++ src/pages/dashboardFavoritesPage/index.tsx
@@ -1,9 +1,64 @@
+import { Card, Skeleton } from "@mui/material";
+import { getFavoritesController } from "backend/controllers/laravel-api/getFavoritesController";
+import { LaravelResponseContent } from "backend/types/ApiExternalResponse";
+import CardMinimized from "components/CardMinimized";
 import { DashboardContainer } from "components/DashboardContainer";
+import { Label } from "components/MoviesLabel";
+import { useEffect, useState } from "react";
 
 export const DashboardFavoritesPage = () => {
+
+    const [cards, setCards] = useState<LaravelResponseContent>({
+        contents: []
+    });
+
+    const envolviment = {
+        isNormalLabel: false,
+        SectionTitle: "Todos os Favoritos",
+        SectionSubTitle: "",
+        NoButton: true
+    };
+
+    const user = localStorage.getItem('user');
+    const userJson = JSON.parse(user!);
+    const userToken = userJson.token;
+
+    useEffect(() => {
+        const controller = new getFavoritesController();
+        controller.handle(userToken).then((response) => {
+            setCards(response.data);
+        });
+    }, []);
+
     return (
         <DashboardContainer>
-            <h1> FAVORITES </h1>
+            <Label envolviment={envolviment} />
+
+            <div className="grid 2xl:grid-cols-6 xl:grid-cols-5 lg:grid-cols-5 sm:grid-cols-3 grid-cols-1 gap-x-6 gap-y-12 w-full mt-6">
+
+                {cards.contents.length > 0 ? (
+                    cards.contents.map((card) => {
+                        return (
+                            <CardMinimized card={card} />
+                        );
+                    })
+                ) :
+                    <>
+                        <Skeleton variant="rectangular" height={290} />
+                        <Skeleton variant="rectangular" height={290} />
+                        <Skeleton variant="rectangular" height={290} />
+                        <Skeleton variant="rectangular" height={290} />
+                        <Skeleton variant="rectangular" height={290} />
+                        <Skeleton variant="rectangular" height={290} />
+                        <Skeleton variant="rectangular" height={290} />
+                        <Skeleton variant="rectangular" height={290} />
+                        <Skeleton variant="rectangular" height={290} />
+                        <Skeleton variant="rectangular" height={290} />
+                        <Skeleton variant="rectangular" height={290} />
+                        <Skeleton variant="rectangular" height={290} />
+                    </>
+                }
+            </div>
         </DashboardContainer>
     );
 }
--- src/pages/dashboardWatchlistPage/index.tsx
+++ src/pages/dashboardWatchlistPage/index.tsx
@@ -1,11 +1,64 @@
+import { Skeleton } from "@mui/material";
+import { getWatchlistController } from "backend/controllers/laravel-api/getWatchlistController";
+import { LaravelResponseContent } from "backend/types/ApiExternalResponse";
+import CardMinimized from "components/CardMinimized";
 import { DashboardContainer } from "components/DashboardContainer";
-import Footer from "components/Footer";
-import { OtherNavBar } from "components/NavBar";
+import { Label } from "components/MoviesLabel";
+import { useEffect, useState } from "react";
 
 export const DashboardWatchlistPage = () => {
+
+    const [cards, setCards] = useState<LaravelResponseContent>({
+        contents: []
+    });
+
+    const envolviment = {
+        isNormalLabel: false,
+        SectionTitle: "Todos da Lista De Espera",
+        SectionSubTitle: "",
+        NoButton: true
+    };
+
+    const user = localStorage.getItem('user');
+    const userJson = JSON.parse(user!);
+    const userToken = userJson.token;
+
+    useEffect(() => {
+        const controller = new getWatchlistController();
+        controller.handle(userToken).then((response) => {
+            setCards(response.data);
+        });
+    }, []);
+
     return (
         <DashboardContainer>
-            <h1> WATCHLIST </h1>
+            <Label envolviment={envolviment} />
+
+            <div className="grid 2xl:grid-cols-6 xl:grid-cols-5 lg:grid-cols-5 sm:grid-cols-3 grid-cols-1 gap-x-6 gap-y-12 w-full mt-6">
+
+                {cards.contents.length > 0 ? (
+                    cards.contents.map((card) => {
+                        return (
+                            <CardMinimized card={card} />
+                        );
+                    })
+                ) :
+                    <>
+                        <Skeleton variant="rectangular" height={290} />
+                        <Skeleton variant="rectangular" height={290} />
+                        <Skeleton variant="rectangular" height={290} />
+                        <Skeleton variant="rectangular" height={290} />
+                        <Skeleton variant="rectangular" height={290} />
+                        <Skeleton variant="rectangular" height={290} />
+                        <Skeleton variant="rectangular" height={290} />
+                        <Skeleton variant="rectangular" height={290} />
+                        <Skeleton variant="rectangular" height={290} />
+                        <Skeleton variant="rectangular" height={290} />
+                        <Skeleton variant="rectangular" height={290} />
+                        <Skeleton variant="rectangular" height={290} />
+                    </>
+                }
+            </div>
         </DashboardContainer>
     );
 }
