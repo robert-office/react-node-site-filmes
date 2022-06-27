e707edc0db2bf55d60eb21e90c57283b5bbdd033
robert-office <robertuillians123@gmail.com>
2022-06-27 15:18:22 -0300

    resolvido o problema do trailler?

--- src/components/Trailler/index.tsx
+++ src/components/Trailler/index.tsx
@@ -10,14 +10,14 @@ export const Trailler = ({ traillerData }: Props) => {
     <object width="100%" height="100%" className="relative h-96 min-w-full">
       <param
         name="movie"
-        value={`http://www.youtube.com/embed/${traillerData.key}?html5=1&amp;rel=0&amp;hl=en_US&amp;version=3`}
+        value={`https://www.youtube.com/embed/${traillerData.key}?html5=1&amp;rel=0&amp;hl=en_US&amp;version=3`}
       />
       <param name="allowFullScreen" value="true" />
       <param name="allowscriptaccess" value="always" />
       <embed
         width="640"
         height="360"
-        src="http://www.youtube.com/embed/yt-video-id?html5=1&amp;rel=0&amp;hl=en_US&amp;version=3"
+        src="https://www.youtube.com/embed/yt-video-id?html5=1&amp;rel=0&amp;hl=en_US&amp;version=3"
         className="youtube-player"
         type="text/html"
       />
